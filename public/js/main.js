a2e7c04fca27abbad87742fce831e7c22ede5347
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-11-09 22:41:19 -0200

    produto

--- public/js/main.js
+++ public/js/main.js
@@ -47,10 +47,31 @@ if (el_txt_email) {
 
 let lista_itens = document.querySelector('#lista-items')
 if (lista_itens) {
+    if (sessionStorage.getItem('produtos')) {
+        let produtos = sessionStorage.getItem('produtos');
+
+        lista_itens.innerHTML = "";
+                    produtos.forEach((produto) => {
+                        lista_itens.innerHTML += `<div class="col-3">
+                                                    <div class="card">
+                                                        <div class="card-header">
+                                                            <h3 class="card-title">${produto.title}</h3>
+                                                        </div>
+                                                        <img class="card-img" src="${produto.image}" width="100" height="200" />
+                                                        <div class="card-footer text-center d-flex justify-content-between">
+                                                            <h4>R$ ${produto.price}</h4>
+                                                            <a href="/produto/${produto.id}" class="btn btn-primary">Comprar</a>
+                                                        </div>
+                                                    </div>
+                                                </div>`;
+                        // equivalente: lista_itens.innerHTML = lista_itens.innerHTML.concat(`<div class="col-3">...
+                    })
+    } else {
     fetch('https://fakestoreapi.com/products')
                 .then(res=>res.json())
                 .then( produtos => {
                     console.log(produtos);
+                    sessionStorage.setItem('produtos', JSON.stringify(produts));
                     lista_itens.innerHTML = "";
                     produtos.forEach((produto) => {
                         lista_itens.innerHTML += `<div class="col-3">
@@ -68,6 +89,55 @@ if (lista_itens) {
                         // equivalente: lista_itens.innerHTML = lista_itens.innerHTML.concat(`<div class="col-3">...
                     })
                 })
+            }
 }
 // produto_id
-// Carregar a url da api https://fakestoreapi.com/products/ID_DA_URL
+// Carregar a url da api 
+
+fetch(`https://fakestoreapi.com/products/${produto_id}`)
+    .then(
+        (response) => response.json())
+    .then(
+        produto => {
+            console.log(produto)
+            let elemento_titulo = document.querySelector('#titulo')
+            elemento_titulo.innerHTML = produto.title;
+
+            let elemento_imagem = document.querySelector('#imagem')
+            elemento_imagem.src = produto.image;
+
+            let elemento_descricao = document.querySelector('#descricao')
+            elemento_descricao.innerHTML = produto.description;
+
+            let elemento_preco = document.querySelector('#preco')
+            elemento_preco = produto.preco
+
+            let elemento_add_carrionho = document.querySelector('#adicionar_carrinho')
+            elemento_add_carrionho.href = "/produto/"+produto.id
+
+            // let elemento = document.querySelector('#produto')
+            // elemento.innerHTML = `
+            // <div class="card my-5">
+            //     <div class="card-header">
+            //         <h3 class="card-title">${produto.title}</h3>
+            //     </div>
+            //     <div class="card-body">
+            //         <div class="row">
+            //             <div class="col-6">
+            //                 <img class="card-img" src="${produto.image}" width="100" height="400" />
+            //             </div>
+            //             <div class="col-6">
+            //                 <p>${produto.description}</p>
+
+            //                 <h4>R$ ${produto.price}</h4>
+            //             </div>
+            //         </div>
+            //     </div>
+            //     <div class="card-footer text-right">
+            //         <a href="/produto/${produto.id}" class="btn btn-outline-primary">+ Adicionar ao carrinho</a>
+            //     </div>
+            // </div>
+            // `
+        }
+    )
+
